35fecb11a403ab83c6394820168d9f3724b2c354
JasonLiJT <JasonLiJT@users.noreply.github.com>
2021-12-24 12:11:46 +0000

    Refactor

--- 24Game.cpp
+++ 24Game.cpp
@@ -4,37 +4,48 @@
 #include <cmath>
 #include <algorithm>
 #include <set>
+#include <array>
 
 using namespace std;
 
 constexpr double PI = 3.14159265358979323846;
-double transcendental[4] = {PI, exp(1), log(PI), exp(PI)};
+std::array<double, 4> transcendental = {PI, exp(1), log(PI), exp(PI)};
+
+bool isClose(double a, double b, double thresh = 1e-6) {
+    return fabs(a - b) < thresh;
+}
 
 struct Card {
-    double num;
-    double symbol;
+    double num = 0;
+    double symbol = 0;
+    void set(double num_, double transcendental) {
+        num = num_;
+        if (abs(num) == 1 || num == 0) {
+            symbol = num;
+        } else {
+            symbol = transcendental;
+        }
+    }
     bool operator<(const Card& other) const {
         return num < other.num;
     }
-    Card operator+(const Card& other) const {
-        return Card{num + other.num, symbol + other.symbol};
-    }
-    Card operator-(const Card& other) const {
-        return Card{num - other.num, symbol - other.symbol};
-    }
-    Card operator*(const Card& other) const {
-        return Card{num * other.num, symbol * other.symbol};
-    }
-    Card operator/(const Card& other) const {
-        return Card{num / other.num, symbol / other.symbol};
-    }
+#define CARD_OP_DEF(op)                                    \
+Card operator op(const Card& other) const {                \
+    return Card{num op other.num, symbol op other.symbol}; \
+}
+    CARD_OP_DEF(+);
+    CARD_OP_DEF(-);
+    CARD_OP_DEF(*);
+    CARD_OP_DEF(/);
+#undef CARD_OP_DEF
 };
 
 struct Expression {
     string str;
-    double symbolEigen;
+    double symbolEigen = 0;
+    bool hasNonIntSubResult = false;
     bool operator<(const Expression& other) const {
-        if (fabs(symbolEigen - other.symbolEigen) < 1e-6) {  // Equal
+        if (isClose(symbolEigen, other.symbolEigen)) {  // Equal
             return false;
         }
         return symbolEigen < other.symbolEigen;
@@ -43,14 +54,10 @@ struct Expression {
 
 Card calculateFromOperator(const Card& left, const Card& right, char op) {
     switch (op) {
-        case '+':
-            return left + right;
-        case '-':
-            return left - right;
-        case '*':
-            return left * right;
-        case '/':
-            return left / right;
+        case '+': return left + right;
+        case '-': return left - right;
+        case '*': return left * right;
+        case '/': return left / right;
         default:
             cerr << "Error! Wrong symbol: " << op << endl;
             throw -1;
@@ -60,54 +67,64 @@ Card calculateFromOperator(const Card& left, const Card& right, char op) {
 class TwentyFourGameSolver {
    private:
     set<Expression> solutions;  // set<Expression> will ignore equivalent expressions
-    Card cards[4] = {};
-    char operations[4];
-    int operationSlots[3];
-    int precedence[3];
+    std::array<Card, 4> cards = {};
+    std::array<char, 4> operations = {'+', '-', '*', '/'};
+    std::array<int, 3> operationSlots = {};
+    std::array<int, 3> precedence = {0, 1, 2};
     double target;
+    bool hardSolutionsOnly;
    public:
-    TwentyFourGameSolver() : operations{'+', '-', '*', '/'},
-                             operationSlots{},
-                             precedence{0, 1, 2} {
+    static std::array<double, 5> readInput() {
         cout << "Please enter 4 cards in [1, 13] (e.g. 1 4 5 6) followed by an optional target (default 24):" << endl;
-        readInput();
+        std::array<double, 5> inputs = {};
+        string tmp;
+        getline(cin, tmp);
+        stringstream ss(tmp);
+        for (int i = 0; i < 4; ++i) {
+            ss >> inputs[i];
+        }
+        if (!(ss >> inputs[4])) {
+            inputs[4] = 24;
+        }
+        return inputs;
     }
 
-    void readInput() {
-        string input;
-        stringstream ss;
-        getline(cin, input);
-        ss << input;
+    TwentyFourGameSolver() : TwentyFourGameSolver(readInput()) {}
+    TwentyFourGameSolver(std::array<double, 5> inputs, bool hardSolutionsOnly = false)
+        : operations{'+', '-', '*', '/'}
+        , operationSlots{}
+        , precedence{0, 1, 2}
+        , target(inputs[4])
+        , hardSolutionsOnly(hardSolutionsOnly)
+    {
         for (int i = 0; i < 4; ++i) {
-            ss >> cards[i].num;
-            if (abs(cards[i].num) == 1 || cards[i].num == 0) {
-                cards[i].symbol = cards[i].num;
-            } else {
-                cards[i].symbol = transcendental[i];
-            }
-        }
-        if (!(ss >> target)) {
-            target = 24;
+            cards[i].set(inputs[i], transcendental[i]);
         }
     }
 
     void solve() {
         // Make sure duplicate cards have the same symbol
-        sort(cards, cards + 4);
+        sort(begin(cards), end(cards));
         for (int i = 1; i < 4; ++i) {
             if (cards[i - 1].num == cards[i].num) {
                 cards[i].symbol = cards[i - 1].symbol;
             }
         }
-        sort(precedence, precedence + 3);
+        sort(begin(precedence), end(precedence));
         do {  // Permutate cards
             solvePermutedCards();
-        } while (next_permutation(cards, cards + 4));
+        } while (next_permutation(begin(cards), end(cards)));
 
         printSolutions();
     }
 
     void printSolutions() {
+        if (hardSolutionsOnly && (solutions.empty()
+            || any_of(begin(solutions), end(solutions), [](const auto& e) { return !e.hasNonIntSubResult; }))
+        ) {
+            cerr << cards[0].num << ' ';
+            return;
+        }
         if (solutions.empty()) {
             cout << "No solutions!" << endl;
         } else {
@@ -128,17 +145,18 @@ class TwentyFourGameSolver {
             }
             do {  // Permutate precedence
                 solvePermutedPrecedence();
-            } while (next_permutation(precedence, precedence + 3));
+            } while (next_permutation(begin(precedence), end(precedence)));
         }
     }
 
     void solvePermutedPrecedence() {
-        int calculated[4] = {};
-        string subExpressions[4] = {};
+        std::array<int, 4> calculated = {};
+        std::array<string, 4> subExpressions = {};
         Expression expression;
-        Card subResults[4];
-        copy(cards, cards + 4, subResults);
+        std::array<Card, 4> subResults;
+        copy(begin(cards), end(cards), begin(subResults));
         double result;
+        bool hasNonIntSubResult = false;
         for (int i = 0; i < 3; ++i) {
             int current = precedence[i];
             Card left, right;
@@ -169,12 +187,18 @@ class TwentyFourGameSolver {
                 tmpExpression << ")";
             }
             subResults[current] = calculateFromOperator(left, right, operations[operationSlots[current]]);
+            if (double num = subResults[current].num;
+                !hasNonIntSubResult && !isClose(num, round(num))
+            ) {
+                hasNonIntSubResult = true;
+            }
 
             string tmpStr = tmpExpression.str();
             if (i == 2) {
                 result = subResults[current].num;
                 expression.str = tmpStr;
                 expression.symbolEigen = subResults[current].symbol;
+                expression.hasNonIntSubResult = hasNonIntSubResult;
                 break;
             }
             for (int j = 0; j < 4; ++j) {
@@ -184,14 +208,28 @@ class TwentyFourGameSolver {
                 }
             }
         }
-        if (fabs(result - target) < 1e-6) {
+        if (isClose(result, target)) {
             // set<Expression> will ignore equivalent expressions
-            solutions.insert(expression);
+            solutions.insert(std::move(expression));
         }
     }
 };
 
+void findHardCards(double max_card = 13, double target = 24) {
+    for(double i = 1; i <= max_card; ++i) {
+        for(double j = i; j <= max_card; ++j) {
+            for(double k = j; k <= max_card; ++k) {
+                for(double l = k; l <= max_card; ++l) {
+                    TwentyFourGameSolver solver{{i, j, k, l, target}, true};
+                    solver.solve();
+                }
+            }
+        }
+    }
+}
+
 int main() {
     TwentyFourGameSolver solver;
     solver.solve();
+    // findHardCards();
 }
--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 Get all algebraically unique solutions to the 24 Game in C++. https://repl.it/@lijt931/24-Game-Solver
 
 ## Usage and Demo
-Run directly [here](https://repl.it/@lijt931/24-Game-Solver), or compile in C++11 or above and run.
+Run directly [here](https://repl.it/@lijt931/24-Game-Solver), or compile in C++17 or above and run.
 
 Follow the instructions:
-```C++
+```
 Please enter 4 cards in [1, 13] (e.g. 1 4 5 6) followed by an optional target (default 24):
 1 2 3 4
 #1: (1+3)*(2+4) = 24
@@ -15,13 +15,12 @@ Please enter 4 cards in [1, 13] (e.g. 1 4 5 6) followed by an optional target (d
 
 Note that other equivalent solutions like `4*3*2*1` are omitted.
 
-The cards and the target can be arbitrary integers, for example:
+The cards and the target can be arbitrary real numbers, for example:
 
-```C++
+```
 Please enter 4 cards in [1, 13] (e.g. 1 4 5 6) followed by an optional target (default 24):
--6 -7 8 9 25
-#1: (-7*-6)-(8+9) = 25
-#2: ((-6+8)*9)--7 = 25
+-1 -2.5 3 4.5 5.5
+#1: (-2.5*(-1-3))-4.5 = 5.5
 ```
 
 ## Algorithm
@@ -29,8 +28,8 @@ The algorithm consists of two parts.
 The first part use brute-force to generate all possible solutions and the second part eliminates equivalent ones.
 
 ### 1 - Brute-force Solutions
-Each permutation of the 4 cards is generated, then each permutation of the operands (+, -, *, /), then each permutation of precedence.
+Each permutation of the 4 cards is traversed, then each permutation of the operands (+, -, *, /), then each permutation of precedence.
 
 ### 2- Eliminate equivalent expressions
 One way to achieve this is to use a polynomial class and define the equivalence criterion.
-This algorithm uses a simpler way, which is assigning a transcendental number (e.g. PI, e) to each card value, and compare the results.
+This algorithm uses a simpler way, which is assigning a transcendental number (e.g. PI, e) to each card value, and compare the arithmetic results.
